535f3cb78d438c752778d573bac1d4c0ef90d264
William Clifford <bcliffor@systemsbiology.org>
2018-11-28 15:45:30 -0800

    Don't need to call loadMetadata at startup

--- initState.py
+++ initState.py
@@ -5,35 +5,50 @@ import json
 
 # Build a list of keywords
 def loadKeywords(args):
-    with open(args.keywords) as f:
-        strings = f.read().splitlines()
-        keywords = set(strings)
-        print("keywords {}".format(keywords))
+    if os.path.exists(args.keywords):
+        with open(args.keywords) as f:
+            strings = f.read().splitlines()
+            keywords = set(strings)
+            print("keywords {}".format(keywords))
+    else:
+        keywords = set()
     return keywords
 
 # Build a list of ignored keywords
 def loadIgnoredKeywords(args):
-    with open(args.ignoredKeywords) as f:
-        strings = f.read().splitlines()
-        ignoredKeywords = set(strings)
-        print("ignoredKeywords {}".format(ignoredKeywords))
+    if os.path.exists(args.ignoredKeywords):
+        with open(args.ignoredKeywords) as f:
+            strings = f.read().splitlines()
+            ignoredKeywords = set(strings)
+            print("ignoredKeywords {}".format(ignoredKeywords))
+    else:
+        ignoredKeywords = set()
     return ignoredKeywords
 
 
 # Load existing metadata
 def loadMetadata(args):
-    with open(args.metadata) as f:
-        f.seek(0, 2)  # Go to the end of file
-        if f.tell() == 0:  # Check if file is empty
-            metadata = {}
-        else:
-            f.seek(0)
-            strings = f.read()
-            metadata = json.loads(strings)
-    series = metadata.keys()
-    if args.verbosity > 1:
-        print("Have metadata for {} series".format(len(series)))
-    return series
+    if os.path.exists(args.metadata):
+        with open(args.metadata) as f:
+            f.seek(0, 2)  # Go to the end of file
+            if f.tell() == 0:  # Check if file is empty
+                metadata = {}
+            else:
+                f.seek(0)
+                strings = f.read()
+                metadata = json.loads(strings)
+
+# Get the series we've already processed
+def loadDones(args):
+    if os.path.exists(args.dones):
+        with open(args.dones) as f:
+            strings = f.read().splitlines()
+            dones = set(strings)
+            print("dones {}".format(dones))
+    else:
+        dones = set()
+    return dones
+
 
 
 # Build a list of ignored element types
@@ -45,7 +60,7 @@ def loadIgnoredTypes(args):
     return ignoredTypes
 
 
-# Build a list of keywords
+# Build a list of files to process
 def loadZips(args):
     with open(args.zips) as f:
         strings = f.read().splitlines()
@@ -57,7 +72,8 @@ def setup(args):
     keywords = loadKeywords(args)
     ignoredKeywords = loadIgnoredKeywords(args)
     ignoredTypes = loadIgnoredTypes(args)
-    series = loadMetadata(args)
+    #loadMetadata(args)
+    series = loadDones(args)
     zips = loadZips(args)
     return (keywords, ignoredKeywords, ignoredTypes, series, zips)
 
